b0b93c92fab04a24070b85ec494e0ad52319214c
David Fuentes <AntonioFuentes.09.01@gmail.com>
2025-11-12 21:03:17 -0600

    2025-11-12 | Sincronizacion

--- C_51125_ProyClaseQ4/Services/FirebaseServices.cs
+++ C_51125_ProyClaseQ4/Services/FirebaseServices.cs
@@ -12,8 +12,11 @@ namespace C_51125_ProyClaseQ4.Services
 
         public FirebaseServices(IConfiguration configuration)
         {
+            //Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"C:\\Users\\Dafuentes\\Documents\\GitHub\\C_51125_ProyClaseQ4\\C_51125_ProyClaseQ4\\Config\\firebase-credentials.json");
+            //_firestoreDb = FirestoreDb.Create("spqr-opera-base");
             _projectId = configuration["Firebase:ProjectId"]
                 ?? throw new InvalidOperationException("Firebase ProjectId is not configured.");
+
 
             if (FirebaseApp.DefaultInstance == null)
             {
@@ -26,7 +29,6 @@ namespace C_51125_ProyClaseQ4.Services
 
             }
 
-            //Crear instancia de Firebase
             _firestoreDb = FirestoreDb.Create(_projectId);
         }
 
